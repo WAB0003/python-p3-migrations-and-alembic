fb2f2f7b1a177afa5946330918818484d1ac40c8
Bill Brown <bill.a.brown90@gmail.com>
2023-04-17 13:08:43 -0700

    migrations and alembic

---
+++ lib/migrations/versions/488dd45041f2_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 488dd45041f2
+Revises: 
+Create Date: 2023-04-17 12:49:21.691163
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '488dd45041f2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
